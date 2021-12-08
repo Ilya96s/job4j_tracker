92b8e1ee3dacc4fe341ce41431c2d10355cad4ab
Ilya96s <i.kaltygin@icloud.com>
2021-12-08 19:58:45 +0300

    1.2. Вызов метода объекта [#166085]

---
+++ src/main/java/oop/Student.java
@@ -0,0 +1,13 @@
+package oop;
+
+public class Student {
+    public void music() {
+        System.out.println("I believe i can fly");
+    }
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music();
+        petya.music();
+        petya.music();
+    }
+}
