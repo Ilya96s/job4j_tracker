35da71e7b6114dba69a0b89ac9f4dbc87aa5e351
Ilya96s <i.kaltygin@icloud.com>
2021-12-09 20:14:14 +0300

    1.5. Поля объекта [#166083]

---
+++ src/main/java/oop/Cat.java
@@ -0,0 +1,33 @@
+package oop;
+
+public class Cat {
+
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food");
+        Cat gav = new Cat();
+        gav.name = ("gav");
+        gav.eat("Kotletka");
+        gav.show();
+        System.out.println("There are black's food");
+        Cat black = new Cat();
+        black.name = ("black");
+        black.eat("fish");
+        black.show();
+    }
+}
