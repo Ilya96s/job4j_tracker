379fadb1c701ec917dc84de6c9c0cddf09621682
Ilya96s <i.kaltygin@icloud.com>
2021-12-11 12:35:01 +0300

    Конструктор

---
+++ src/main/java/oop/Error.java
@@ -0,0 +1,36 @@
+package oop;
+
+public class Error {
+
+    private boolean active;
+
+    private int status;
+
+    private String message;
+
+    public Error(boolean active, int status, String message){
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error(){
+    }
+
+    public void info() {
+        System.out.println("Наличие ошибки : " + active);
+        System.out.println("Статус : " + status);
+        System.out.println("Сообщение : " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error(true, 400, "Неверный запрос");
+        Error error2 = new Error(true, 1, "Доступ к ресурсу запрещен");
+        Error error3 = new Error(true, 404, "Ресурс не найден");
+        Error error4 = new Error();
+        error1.info();
+        error2.info();
+        error3.info();
+        error4.info();
+    }
+}
