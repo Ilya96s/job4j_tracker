687579ae5eaa6228a2a61a957b05787dbcd37d1a
Ilya96s <i.kaltygin@icloud.com>
2021-12-12 16:57:58 +0300

    Рефакторинг - Расстояние между точками. V2

--- src/main/java/oop/Point.java
+++ src/main/java/oop/Point.java
@@ -19,8 +19,8 @@ public class Point {
     }
 
     public static void main(String[] args) {
-        Point a = new Point(0,0);
-        Point b = new Point(0,2);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
         double dist = a.distance(b);
         System.out.println(dist);
     }
