87ea59dae48d9a6cd494685880ba6df7a06ddeae
Ilya96s <i.kaltygin@icloud.com>
2021-12-13 19:46:18 +0300

    Рефакторинг - Площадь треугольника.

---
+++ src/main/java/oop/Triangle.java
@@ -0,0 +1,34 @@
+package oop;
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double semiPerimetr(double a, double b, double c) {
+        double rsl = (a + b + c) / 2;
+        return rsl;
+    }
+
+    public boolean exist(double ab, double ac, double bc) {
+        return (ab + ac > bc) && (ac + bc > ab) && (ab + bc > ac);
+    }
+
+    public double area() {
+        double rsl = -1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        if (this.exist(ab, ac, bc)) {
+            double p = semiPerimetr(ab, ac, bc);
+            rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
+        }
+        return rsl;
+    }
+}
