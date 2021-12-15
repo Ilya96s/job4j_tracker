d76e6899a01297d44908b0f1ae94759a957ffa00
Ilya96s <i.kaltygin@icloud.com>
2021-12-15 21:47:55 +0300

    Рефакторинг - Площадь треугольника. Добавил тест, когда нельзя построить треугольник

--- src/test/java/oop/TriangleTest.java
+++ src/test/java/oop/TriangleTest.java
@@ -2,6 +2,7 @@ package oop;
 
 import static org.hamcrest.Matchers.closeTo;
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.Test;
 
@@ -15,5 +16,15 @@ public class TriangleTest {
         Triangle triangle = new Triangle(a, b, c);
         double rsl = triangle.area();
         assertThat(rsl, closeTo(8, 0.001));
+    }
+
+    @Test
+    public void area1() {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        Point c = new Point(0, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, is(-1.0));
     }
 }
