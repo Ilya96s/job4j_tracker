7210d7ec1425a7fd801ecbdb738b2d9587796339
Ilya96s <i.kaltygin@icloud.com>
2021-12-18 20:49:32 +0300

    Переопределение. Добавил тест

--- src/main/java/ru/job4j/inheritance/HtmlReport.java
+++ src/main/java/ru/job4j/inheritance/HtmlReport.java
@@ -1,9 +1,10 @@
 package ru.job4j.inheritance;
 
-public class HtmlReport extends TextReport {
+public class HtmlReport extends  TextReport {
     public String generate(String name, String body) {
-        return "<h1>" + name + "</h1>" +
-                "<br/>" +
-                "<span>" + body + "</span>";
+        return "<h1>" + name + "</h1>"
+                + "<br/>"
+                + "<span>" + body + "</span>";
     }
-}
+    }
+
