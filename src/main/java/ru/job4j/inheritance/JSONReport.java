169c141e9b29eabbb0c1dc227d367c412479f040
Ilya96s <i.kaltygin@icloud.com>
2021-12-19 12:02:08 +0300

    Переопределение

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -3,9 +3,9 @@ package ru.job4j.inheritance;
 public class JSONReport extends TextReport {
     @Override
     public String generate(String name, String body) {
-        return "{" + System.lineSeparator()
-                + "\t\"name\" : \"Report's name\"," + System.lineSeparator()
-                + "\t\"body\" : \"Report's body\"" + System.lineSeparator()
-               + "}";
+        return  "{" + System.lineSeparator()
+                + "\t\"" + "name" + "\"" + " : " + "\"" + name + "\"" + "," + System.lineSeparator()
+                + "\t\"" + "body" + "\"" + " : " + "\"" + body + "\"" + "," + System.lineSeparator()
+                + "}";
     }
 }
