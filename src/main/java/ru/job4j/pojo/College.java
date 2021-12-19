18e3082e846818879d567b7fd73c5227173e5961
Ilya96s <i.kaltygin@icloud.com>
2021-12-19 13:17:34 +0300

    Модель данных

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,11 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFullName("Иванов Иван Иванович");
+        student.setGroup("ЭМм-51");
+        student.setStartDate("2012.09.01");
+        System.out.println("Студент: " + student.getFullName() + System.lineSeparator() + "Группа: " + student.getGroup() +System.lineSeparator() + "Дата поступления: " + student.getStartDate());
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,31 @@
+package ru.job4j.pojo;
+
+public class Student {
+    private String fullName;
+    private String group;
+    private String startDate;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+}
