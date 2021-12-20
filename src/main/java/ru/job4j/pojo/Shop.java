84f0f8021c7f3ec7d9f18ac70a8e8e58ec1f1725
Ilya96s <i.kaltygin@icloud.com>
2021-12-20 20:20:56 +0300

    Массив с пустыми ячейками

---
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+
+public class Shop {
+    public static int indexOfNull(Product[] products) {
+        int rsl = -1;
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] == null) {
+                rsl = i;
+                break;
+
+            }
+        }
+        return rsl;
+    }
+}
