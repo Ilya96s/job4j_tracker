ff5682b73294ff8041f7f5a8aa0d330708e4308a
Ilya96s <i.kaltygin@icloud.com>
2021-12-23 18:59:31 +0300

    Что такое валидация?

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -66,14 +66,12 @@ public class Tracker {
 
     public boolean delete(int id) {
         int index = indexOf(id);
-        boolean rsl = index == -1;
+        boolean rsl = index != -1;
         if (rsl) {
-            return false;
-        } else {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
-            return true;
         }
+        return rsl;
     }
 }
