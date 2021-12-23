5cbe39c112c142c7cfd050a28ed5a7e3490479fa
Ilya96s <i.kaltygin@icloud.com>
2021-12-23 19:49:00 +0300

    toString

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,6 +7,7 @@ public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public LocalDateTime getCreated() {
         return created;
@@ -38,5 +39,16 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String currentDateTimeFormatter = created.format(formatter);
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", created=" + currentDateTimeFormatter +
+                '}';
     }
 }
