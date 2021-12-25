8571519ff0ade5f20a279ba04b82ab226e6e9ad6
Ilya96s <i.kaltygin@icloud.com>
2021-12-25 16:18:39 +0300

    Полиморфизм. Реализация класса StartUI. Вывод меню

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,15 +1,34 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class StartUI {
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Select: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
+                run = false;
+            }
+        }
+    }
+
+    private void showMenu() {
+        String[] menu = {"Add new Item", "Show all itemss", "Edit item",
+        "Delete item", "Find item by id", "Find items by name", "Exit program"};
+        System.out.println("Menu");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        Item item = new Item();
-        System.out.println(item);
-        LocalDateTime currentDateTime = item.getCreated();
-        DateTimeFormatter formater = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String currentDateTimeFormat = currentDateTime.format(formater);
-        System.out.println("Текущие дата и время: " + currentDateTimeFormat);
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 }
