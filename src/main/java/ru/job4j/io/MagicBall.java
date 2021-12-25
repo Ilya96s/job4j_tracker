b02a33030cb2c2c4cb4272a4498ba3b1c1e5509f
Ilya96s <i.kaltygin@icloud.com>
2021-12-25 15:22:19 +0300

    Полиморфизм. Чтение из консоли. Класс Scanner

---
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Я великий Оракул. Что ты хочешь узнать ? ");
+        String question = input.nextLine();
+        int answer = new Random().nextInt(3);
+        if (answer == 0) {
+            System.out.println("Да");
+        } else if (answer == 1) {
+            System.out.println("Нет");
+        } else {
+            System.out.println("Может быть");
+        }
+        System.out.println(answer);
+    }
+}
