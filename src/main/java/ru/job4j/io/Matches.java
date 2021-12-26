b8f20a5582e66ab2c26596b5ce0ba2a34d9b34ed
Ilya96s <i.kaltygin@icloud.com>
2021-12-26 14:54:29 +0300

    Полиморфизм. Scanner и чтение числа из консоли

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -12,9 +12,17 @@ public class Matches {
             String player = turn ? "Первый игрок" : "Второй игрок";
             System.out.println(player + " введите число от 1 до 3:");
             int matches = Integer.parseInt(input.nextLine());
-             count -= matches;
-            System.out.println("Осталось " + count + " спичек");
-            turn = !turn;
+            if (matches > 0 && matches < 4) {
+                count -= matches;
+                System.out.println("Осталось " + count + " спичек");
+                turn = !turn;
+                if (!turn) {
+                    System.out.println("Выиграл первый игрок");
+                } else {
+                    System.out.println("Выиграл второй игрок");
+                }
+            }
         }
     }
 }
+
