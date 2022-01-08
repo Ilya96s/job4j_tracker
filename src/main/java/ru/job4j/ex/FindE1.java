ff9d90558d8834574ea759d426b82eddb1b4e897
Ilya96s <i.kaltygin@icloud.com>
2022-01-08 14:26:09 +0300

    Исключения. Пользовательские исключения

--- src/main/java/ru/job4j/ex/FindE1.java
+++ src/main/java/ru/job4j/ex/FindE1.java
@@ -6,6 +6,7 @@ public class FindE1 {
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 rsl = i;
+                break;
             }
         }
         if (rsl == -1) {
