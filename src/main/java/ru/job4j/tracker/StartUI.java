7bbe34f0b1d254b42473d2418783cafeb0bf9dfe
Ilya96s <i.kaltygin@icloud.com>
2022-01-09 12:49:09 +0300

    Исключения. Обеспечить бесперебойную работу приложения Tracker

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -14,7 +14,7 @@ public class StartUI {
         while (run) {
             this.showMenu(actions);
             int select = input.askInt("Select: ");
-            if (select < 0 || select > actions.length) {
+            if (select < 0 || select >= actions.length) {
                 out.println("Wrong input, you can select: 0 .. " + (actions.length - 1));
                 continue;
             }
