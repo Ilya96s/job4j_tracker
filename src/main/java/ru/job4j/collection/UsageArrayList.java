fbe2aebfb78219c1fbdf55572e2146579b4b5e9a
Ilya96s <i.kaltygin@icloud.com>
2022-01-20 21:15:51 +0300

    Коллекция List, ArrayList. 0. Коллекции, ArrayList, List, Обобщения

---
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> person = new ArrayList<String>();
+        person.add("Petr");
+        person.add("Ivan");
+        person.add("Stepan");
+        for (Object obj : person) {
+            System.out.println(obj);
+        }
+    }
+}
