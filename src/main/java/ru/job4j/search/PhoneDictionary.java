58125d32531f9bc66047b255792c1db739d593ab
Ilya96s <i.kaltygin@icloud.com>
2022-01-21 21:53:27 +0300

    Коллекция List, ArrayList. 1. Телефонный справочник на базе ArrayList

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -13,8 +13,9 @@ public class PhoneDictionary {
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
             if (person.getName().contains(key) || person.getSurname().contains(key)
-                    || person.getPhone().contains(key) || person.getAddress().contains(key));
-            result.add(person);
+                    || person.getPhone().contains(key) || person.getAddress().contains(key)) {
+                result.add(person);
+            }
         }
         return result;
     }
