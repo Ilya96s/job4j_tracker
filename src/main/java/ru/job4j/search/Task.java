a625cfdae742b530ad0f076bcdfbe350ada3438d
Ilya96s <i.kaltygin@icloud.com>
2022-01-22 14:22:00 +0300

    Коллекция List, ArrayList. 2. Очередь с приоритетом на LinkedList

---
+++ src/main/java/ru/job4j/search/Task.java
@@ -0,0 +1,21 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class Task {
+    private String desc;
+    private int priority;
+
+    public Task(String desc, int priority) {
+        this.desc = desc;
+        this.priority = priority;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
