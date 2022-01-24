b08f6631adcf0beaba7dd719f709c64336f75da3
Ilya96s <i.kaltygin@icloud.com>
2022-01-24 20:46:32 +0300

    Коллекция Set, HashSet. 2. Идентичные тексты

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+   public static boolean isEquals(String originText, String deplicateText) {
+       boolean rsl = true;
+       String[] origin = originText.split(" ");
+       String[] text = deplicateText.split(" ");
+       HashSet<String> check = new HashSet<>();
+       for (String s : origin) {
+           check.add(s);
+       }
+       for (String s : text) {
+           rsl = check.contains(s);
+       }
+       return rsl;
+   }
+}
