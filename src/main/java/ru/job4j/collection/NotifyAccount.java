ff7be4ff8dfb6a0aebcc36d80f796bee9dde244c
Ilya96s <i.kaltygin@icloud.com>
2022-01-24 21:21:29 +0300

    Коллекция Set, HashSet. 3. Модели данных и HashSet.

---
+++ src/main/java/ru/job4j/collection/NotifyAccount.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class NotifyAccount {
+    public static HashSet<Account> sent(List<Account> accounts) {
+        HashSet<Account> rsl = new HashSet<>();
+        for (Account s : accounts) {
+            rsl.addAll(accounts);
+        }
+        return rsl;
+    }
+}
