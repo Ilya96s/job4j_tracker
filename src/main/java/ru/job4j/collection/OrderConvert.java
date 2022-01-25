171c0d787b1dabd697d8efb79cb6ef6bb446af60
Ilya96s <i.kaltygin@icloud.com>
2022-01-25 21:37:54 +0300

    Map. HashMap. LinkedHashMap. 2. Преобразования List в Map.

---
+++ src/main/java/ru/job4j/collection/OrderConvert.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class OrderConvert {
+    public static HashMap<String, Order> process(List<Order> orders) {
+        HashMap<String, Order> map = new HashMap<>();
+        for (Order s : orders) {
+            map.put(s.getNumber(), s);
+        }
+        return map;
+    }
+}
