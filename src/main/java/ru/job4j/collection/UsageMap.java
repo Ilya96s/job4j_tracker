be7ca2e3748cf6b34127199e379a96edc70742b2
Ilya96s <i.kaltygin@icloud.com>
2022-01-26 19:53:14 +0300

    Map. HashMap. LinkedHashMap. 0. Map, HashMap

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,7 +7,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> account = new HashMap<>();
         account.put("iva.ivanov@mail.ru", "Ivanov Ivan Ivanovich");
+        account.put("iva.ivanov@mail.ru", "Ivanov Ivan Ivanovich");
         account.put("ex.1@gmail.com", "Blinov Oleg Fedorovich");
+        account.put("111threeunits@mail.ru", "Popov Nikita Sergeevich");
         account.put("111threeunits@mail.ru", "Popov Nikita Sergeevich");
         for (String s : account.keySet()) {
             String value = account.get(s);
