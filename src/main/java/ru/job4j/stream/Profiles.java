5fcd7e7841ad2a07d9dae8716d48db1cfe17be5d
Ilya96s <i.kaltygin@icloud.com>
2022-02-10 18:33:01 +0300

    Stream API. 2. Список адресов

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(profile -> profile.getAddress())
+        return profiles.stream().map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 }
