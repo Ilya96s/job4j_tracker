a76eade3f48728e96490c9523f624b5f11e658ea
Ilya96s <i.kaltygin@icloud.com>
2022-02-14 16:17:32 +0300

    Stream API. 0. Stream API улучшения

--- src/main/java/ru/job4j/stream/Student.java
+++ src/main/java/ru/job4j/stream/Student.java
@@ -15,7 +15,7 @@ public class Student {
 
     public Student(String surname, int score) {
         this.surname = surname;
-        this.score =score;
+        this.score = score;
     }
 
     public int getScore() {
