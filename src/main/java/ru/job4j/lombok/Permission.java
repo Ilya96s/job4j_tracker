b67674b4425e820cedb57ca689ce92c9fafcbddb
Ilya <i.kaltygin@icloud.com>
2022-12-04 13:57:32 +0700

    3. @Builder [#504890]

---
+++ src/main/java/ru/job4j/lombok/Permission.java
@@ -0,0 +1,18 @@
+package ru.job4j.lombok;
+
+import lombok.*;
+
+import java.util.List;
+
+@Builder(builderMethodName = "of")
+@ToString
+@Getter
+public class Permission {
+
+    private int id;
+
+    private String name;
+
+    @Singular("accessBy")
+    private List<String> rules;
+}
